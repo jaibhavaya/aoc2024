7d8363457df79fd58d87db8b50d92aa42d6cff95
Jacob <jaibhavaya@gmail.com>
2025-03-18 00:24:35 -0600

    add day2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,71 @@
+#[derive(Debug)]
+struct Report {
+    entries: Vec<i32>,
+}
+
+impl Report {
+    pub fn is_safe(&self) -> bool {
+        let mut direction = None;
+        for window in self.entries.windows(2) {
+            let change = window[0] - window[1];
+
+            if change.abs() > 3 || change == 0 {
+                return false;
+            }
+
+            let curr_direction = if change < 0 { -1 } else { 1 };
+
+            if let Some(prev_direction) = direction {
+                if curr_direction != prev_direction {
+                    return false;
+                }
+            } else {
+                direction = Some(curr_direction);
+            }
+        }
+        true
+    }
+}
+
+pub fn solve(input: &str) -> i32 {
+    let reports = parse_input(input);
+    reports
+        .iter()
+        .filter(|report| report.is_safe())
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn parse_input(input: &str) -> Vec<Report> {
+    input
+        .lines()
+        .map(|line| -> Report {
+            Report {
+                entries: line
+                    .split_whitespace()
+                    .map(|num| num.parse().unwrap())
+                    .collect(),
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    // This brings all the parent module's items into scope
+    use super::*;
+
+    #[test]
+    fn solve_sample() {
+        let input = "\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9\
+";
+        assert_eq!(solve(input), 2);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 mod day1;
+mod day2;
 
 use std::fs;
 
-use day1::solve;
-use day1::solve2;
-
 fn main() {
     println!("Advent of code 2024");
+
+    // Day 1
     let day1input = fs::read_to_string("inputs/day1").expect("Could not read day1 file");
-    let day1result1 = solve(&day1input);
+
+    let day1result1 = day1::solve(&day1input);
     println!("Day 1 first result: {}", day1result1);
 
-    let day1result2 = solve2(&day1input);
+    let day1result2 = day1::solve2(&day1input);
     println!("Day 1 first result: {}", day1result2);
+
+    // Day2
+    let day2input = fs::read_to_string("inputs/day2").expect("Could not read day2 file");
+
+    let day2result1 = day2::solve(&day2input);
+    println!("Day 2 first result: {}", day2result1);
 }
